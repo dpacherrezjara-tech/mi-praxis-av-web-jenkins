002443f46f8d03e7178ff1e1740b6a04f23f80df
Jesus <jsolano@miatech.net>
2024-10-20 02:58:18 -0500

    controler grafico

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/payments/BalanceAnalysisByAgeController.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/controllers/payments/BalanceAnalysisByAgeController.java
@@ -144,9 +144,11 @@ public class BalanceAnalysisByAgeController extends BaseController {
         System.out.println("-------------- BalanceAnalysisByAge : searchRD-------------");
         map.put("success", true);
         List<A2356Filter> lst = this.getListRD(request, false);
+        List<A2356Filter> lst2 = this.getListRD2(request, false);
         System.out.println("Total : " + lst.size());
         map.put("total", lst.size() > 0 ? lst.get(0).page.TOTROW : 0);
         map.put("data", lst);
+        map.put("data2", lst2);
         return new Gson().toJson(map);
     }
 
@@ -180,6 +182,42 @@ public class BalanceAnalysisByAgeController extends BaseController {
             }
 
             lst = logic.loadSQP05120_RD(filter);
+        } catch (Exception e) {
+            throw new SpringException(e);
+        }
+        return lst;
+    }
+    
+    public List<A2356Filter> getListRD2(HttpServletRequest request, Boolean bExcel) {
+
+        List<A2356Filter> lst = new ArrayList<>(0);
+        A2356Filter filter = new A2356Filter();
+        Gson gson = new Gson();
+        String beanString = "";
+
+        try {
+            logic = new BalanceAnalysisByAgeLogic();
+            logic.setSession(this.serverSession.getServerSession());
+
+            beanString = request.getParameter("beanString");
+            filter = gson.fromJson(beanString, A2356Filter.class);
+            filter.page.TOTROW = -1;
+            filter.page.START = 0;
+            filter.page.LIMIT = 0;
+
+            int limit = request.getParameter("limit") == null ? -1 : Integer.parseInt(request.getParameter("limit").toString());
+            int start = request.getParameter("start") == null ? 0 : Integer.parseInt(request.getParameter("start").toString());
+
+            if (!bExcel) {
+                filter.page.PAGROW = 20;
+                start = (start != 0 ? start : 0);
+                filter.page.PAGNUM = (start / filter.page.PAGROW) + 1;
+            } else {
+                filter.page.PAGROW = -1;
+                filter.page.PAGNUM = 1;
+            }
+
+            lst = logic.loadSQP05120_RD2(filter);
         } catch (Exception e) {
             throw new SpringException(e);
         }
