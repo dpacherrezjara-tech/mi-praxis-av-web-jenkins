fb5df5787dcad4ed7a017d6b693f7693274082c3
Jesus <josue07solano@gmail.com>
2024-11-18 15:57:25 -0500

    Process Control

---
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/ProcessControlForm/Info.js
@@ -0,0 +1,76 @@
+Ext.define('Ext.Praxis.view.payments.ProcessControlForm.Info', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.' + prototype.id + '-info',
+    layout: 'border',
+    align: 'center',
+    bodyStyle: 'background-color: #E3EAEF;',
+    defaults: {
+        bodyStyle: 'background: transparent;',
+        border: false
+    },
+    style: 'margin: 1px;',
+    items: [
+        {
+            region: 'center',
+            layout: {
+                type: 'vbox',
+                align: 'center'
+            },
+            defaults: {
+                bodyStyle: 'background: transparent;',
+                border: false,
+                width: 1400,
+                height: 700,
+                align: 'center'
+            },
+
+            items: [
+                {
+                    xtype: 'panel',
+                    id: prototype.id + '-panelMain',
+                    bodyStyle: 'background-color: #E3EAEF;',
+                    padding: '1',
+                    border: false,
+                    margin: '1',
+                    layout: {
+                        type: 'vbox',
+                        align: 'center'
+                    },
+                    items: [
+                        // --------------------------   GRID MAIN DATA---------------------
+                        {
+                            xtype: 'panel',
+                            id: prototype.id + '-panelGridData',
+                            bodyStyle: 'background-color: #E3EAEF;',
+                            padding: '1',
+                            border: false,
+                            height: 550,
+                            width: 1200,
+                            layout: {
+                                type: 'vbox',
+                                align: 'center'
+                            },
+                            items: [
+                                
+                            ]
+                        }
+                    ]
+                },
+
+                {
+                    region: 'south',
+                    layout: 'border',
+                    height: 0,
+                    defaults: {
+                        style: 'margin: 1px;',
+                        bodyStyle: 'background: transparent;',
+                        border: false
+                    }
+                }
+            ]
+        }
+    ]
+}
+);
+
+
