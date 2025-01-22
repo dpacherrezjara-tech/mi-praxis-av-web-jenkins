5c4db895a051b8e91cc1862d8b79bf4892fbc163
Diego Vicente <diegovic99@hotmail.com>
2025-01-22 11:56:37 -0500

    Ajustes EXT/CO web

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/entities/A4545.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/entities/A4545.java
@@ -48,7 +48,14 @@ public class A4545 {
                 A4545FREGI, 
                 A4545HREGI,
                 A4545DATCI,
-                A4545TRACI; 
+                A4545TRACI,
+                A4545DATEC,
+                A4545TRANC,
+                A4545CCDIG,
+                A4545CCAUTH,
+                A4545CCDAT,
+                A4545MERCH,
+                A4545TSCR; 
                         
     private Integer A4545SEQ,A4545ITEM;
     private BigDecimal A4545ACTIV ,
--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/ReverseAccountingForm/Grids/DetailGrid.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/view/payments/ReverseAccountingForm/Grids/DetailGrid.js
@@ -34,21 +34,25 @@ Ext.define('Ext.Praxis.view.payments.ReverseAccountingForm.Grids.DetailGrid', {
             {text: 'Processor', dataIndex: 'DESC_PRO', width: 180},
             {text: 'Bank Doc.', dataIndex: 'A4545DOCBA', width: 100},
             {text: 'Value<br>Date', dataIndex: 'A4545DOCD', width: 100},
+            {text: 'Reference', dataIndex: 'A4545REFD', width: 160},
             {text: 'Record<br>Type', dataIndex: 'A4545HREGI', width: 80},
+            {text: 'SEQ', dataIndex: 'A4545SEQ', width: 60},
+            {text: 'Item', dataIndex: 'A4545ITEM', width: 60},
             {text: 'Profit', dataIndex: 'A4545PROFI', width: 120},
+            {text: 'Cost Center', dataIndex: 'A4545CCOST', width: 100},
             {text: 'Primary<br>Key', dataIndex: 'A4545PKEY', width: 60,
                 renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
                     const opts = {
-                        '15':()=>{
+                        '15': () => {
                             metaData.style = "color:#2fc611;font-weight:bold;";
                         },
-                        '50':()=>{
+                        '50': () => {
                             metaData.style = "color:#2fc611;font-weight:bold;";
                         },
-                        '40':()=>{
+                        '40': () => {
                             metaData.style = "color:#c61111;font-weight:bold;";
                         },
-                        '01':()=>{
+                        '01': () => {
                             metaData.style = "color:#c61111;font-weight:bold;";
                         }
                     };
@@ -72,24 +76,74 @@ Ext.define('Ext.Praxis.view.payments.ReverseAccountingForm.Grids.DetailGrid', {
                     return value;
                 }
             },
-            {text: 'Item', dataIndex: 'A4545ITEM', width: 60},
             {text: 'Large Text', dataIndex: 'A4545TEXTD', width: 400},
-            {text: 'Reference', dataIndex: 'A4545REFD', width: 160},
-            {text: 'SEQ', dataIndex: 'A4545SEQ', width: 60},
+            {text: 'Bussiness<br>Place', dataIndex: 'A4545PLACE', width: 80},
             {text: 'Bank<br>Code', dataIndex: 'A4545BANCO', width: 70},
             {text: 'Bank Name', dataIndex: 'A4545REFB', width: 180},
             {text: 'Country', dataIndex: 'A4545PAIS', width: 70},
-            {text: 'Place', dataIndex: 'A4545PLACE', width: 80},
+            {text: 'Merchant', dataIndex: 'A4545MERCH', width: 120},
             {text: 'Agent', dataIndex: 'A4545AGENT', width: 90},
-            {text: 'Cost Center', dataIndex: 'A4545CCOST', width: 100},
             {text: 'Key 1', dataIndex: 'A4545REFK', width: 120},
             {text: 'Key 2', dataIndex: 'A4545REFK2', width: 120},
             {text: 'Payment', dataIndex: 'A4545MPAGO', width: 70},
             {text: 'Acc. Number', dataIndex: 'A4545ANUMB', width: 160},
+            {text: 'Sub-Type', dataIndex: 'A4545MODO', width: 100,
+                renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
+                    //metaData.style = "background-color:#838187";
+                    const opts = {
+                        'P': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'PAX CO';
+                        },
+                        'A': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'CGO CO';
+                        },
+                        'C': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'COR CO';
+                        },
+                        'E': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'PAX EXT';
+                        },
+                        'G': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'CGO EXT';
+                        },
+                        'T': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'TAX EXT';
+                        },
+                        'D': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'DEB CO';
+                        },
+                        'B': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'DEB EXT';
+                        },
+                        'J': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'ADJ CO';
+                        },
+                        'K': () => {
+                            metaData.style = "font-weight:bold";
+                            return 'ADJ EXT';
+                        }
+                    };
+                    return opts[value] ? opts[value]() : '';
+                }
+            },
+            {text: 'A. Date', dataIndex: 'A4545ADATE', width: 100},
+            {text: 'Date Bank', dataIndex: 'A4545DATCI', width: 100},
+            {text: 'Trans. Bank', dataIndex: 'A4545TRACI', width: 100},
+            {text: 'Date Settl', dataIndex: 'A4545DATEC', width: 100},
+            {text: 'Trans. Settl', dataIndex: 'A4545TRANC', width: 100},
             {text: 'Accounting<br>Date', dataIndex: 'A4545PSTGD', width: 100},
             {text: 'Accounting<br>ID', dataIndex: 'A4545USER', width: 200},
-            {text: 'Usuario', dataIndex: 'USCR', width: 100},
-            {text: 'Registro<br>Date', dataIndex: 'TSCR', width: 140}
+            {text: 'User', dataIndex: 'USCR', width: 100},
+            {text: 'Reverse<br>Date', dataIndex: 'TSCR', width: 140}
             //</editor-fold>
         ]
     },
