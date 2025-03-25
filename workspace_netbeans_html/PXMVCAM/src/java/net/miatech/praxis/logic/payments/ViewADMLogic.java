c4ad5cb723455704122cdf82dca7cb0b784dbe62
usr_mi_bsaune-at-119260075650 <bsaune@miatech.net>
2025-03-25 12:14:52 -0500

    changes to ADM DISPUTE

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/payments/ViewADMLogic.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/payments/ViewADMLogic.java
@@ -69,6 +69,14 @@ public class ViewADMLogic {
         return ViewADMDAO.loadPX644SQPMPF100ADM_REVERSE(filters, user);
     }
     
+    public String loadPX644SQPMPF100CLEAN_TKT(A2290Filter filter, UserView user) throws SQLException, Exception {
+        return ViewADMDAO.loadPX644SQPMPF100CLEAN_TKT(filter, user);
+    }
+    
+    public String loadPX644SQPMPF100GENERATE_SECOND_ADM(A2290Filter filter, UserView user) throws SQLException, Exception {
+        return ViewADMDAO.loadPX644SQPMPF100GENERATE_SECOND_ADM(filter, user);
+    }
+    
     public List<A2295Filter> loadPX644SQPMPF100ADM_DET(A2295Filter filter) throws SQLException, Exception {
         return ViewADMDAO.loadPX644SQPMPF100ADM_DET(filter);
     }
