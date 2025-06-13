ca103b0fdf1b951608dca3db899d05d1a1b7fdb1
Diego Vicente <diegovic99@hotmail.com>
2025-06-13 12:10:52 -0500

    Rechazos Contables
    Cambios a pantalla Header Report

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/HeadersReport/HeadersGridController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/HeadersReport/HeadersGridController.js
@@ -16,35 +16,7 @@ Ext.define('Ext.Praxis.controller.payments.HeadersReport.HeadersGridController',
     getData: function () {
         const me = this;
         const view = me.view;
-        let store = Ext.create('Ext.data.Store', {
-            loadMask: true,
-            pageSize: 20,
-            proxy: {
-                type: 'ajax',
-                enablePaging: true,
-                url: `${me.url}/loadHeaders`,
-                extraParams: view.searchParams,
-                timeout: 600000,
-                reader: {
-                    type: 'json',
-                    rootProperty: 'response',
-                    totalProperty: 'total'
-                }
-            },
-            autoLoad: true,
-            listeners: {
-                load: function (store, records, successful, operation) {
-                    if (!successful) {
-                        global.Msg({msg: 'Data not Found'});
-                    } else {
-                        //console.log(records);
-                        if (records.length === 0) {
-                            global.Msg({msg: 'Data not Found'});
-                        }
-                    }
-                }
-            }
-        });
+        let store = global.callStorePaggin('PRAXISMP','SPHRP001',view.searchParams);
         view.setStore(store);
     },
     onUpdateRec: function (grid, td, rowIndex, cellIndex, e, record, tr, eOpts) {
