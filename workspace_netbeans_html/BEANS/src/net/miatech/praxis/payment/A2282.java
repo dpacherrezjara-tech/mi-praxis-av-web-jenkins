1f8a1ad3d35be51bc474d6d4425afe85ee19d7f6
SRJAldair <132116143+SRJAldair@users.noreply.github.com>
2025-08-12 15:17:24 -0500

    change Filter

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/A2282.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/A2282.java
@@ -4,6 +4,8 @@
  */
 package net.miatech.praxis.payment;
 
+import net.miatech.beans.Pagination;
+
 /**
  *
  * @author andrea
@@ -36,4 +38,135 @@ public class A2282 {
     public String USUP = "";
     public String FEUP = "";
     public String HOUP = "";
+
+    
+    public int PAGNUM = -1;
+    public int PAGROW = -1;
+    public int TOTPAG = -1;
+    public int TOTROW = -1;
+    public int PAGINIT = -1;
+    /**
+     * Paginacion Extjs
+     */
+    public int TOTROWS = -1;
+    public int START = 0;
+    public int LIMIT = 20;
+    public int PAGE = 1;
+    public Pagination page = new Pagination();
+    public String IN_OPCION = "";
+    public String A4623STATI = "";
+    public String A4623CNUMB = "";
+    public String A4623PLACA = "";
+    public String A4623TKT = "";
+    public String A4623PAX = "";
+    public String A4623DLNAM = "";
+    public String A4623DSNAM = "";
+    public String A4623DFNAM = "";
+    public String A4623DMNAM = "";
+    public String A4623DNIDE = "";
+    public String A4623UCUCO = "";
+    public String A4623UACNA = "";
+    public String A4623UACNU = "";
+    public String A4623ROUCD = "";
+    public String A4623MICUR = "";
+    public String A4623MIREM = "";
+    public String A4623RFNDP = "";
+    public String A4623PNR = "";
+    public String ESTADO = "";
+    public String MDVTA = "";
+    public String MTOVTA = "";
+    public String A4623ANIO = "";
+    public String A4623CCUST = "";
+    public String A4623FCARG = "";
+    public String A4623UIBAN = "";
+    public String A4623DCOUT = "";
+    public String A4623OCHAM = "";
+    public String A4623OSTAM = "";
+    public String A4623DBNM = "";
+    public String BANCO_NAME = "";
+    public String CCIA = "";
+    public String CIAAL = "";
+    public String RBSP = "";
+    public String LANGU = "";
+    public String LANGUC = "";
+    public String COUNTRYS = "";
+    public String USERCR = "";
+    public String PASSCR = "";
+    public String AIDOFIC = "";
+    public String USERCRB = "";
+    public String PASSCRB = "";
+    public String option = "";
+    public String APLIC = "";
+    public String SEQNUM = "" ;
+    public String DENV = "";
+    public String INPNAME = "";
+    public String NETDIR = "";
+    public String INPEXTE = "";
+    public String INPDESC = "";
+    public String INPTYPE = "";
+    public String LIBNAME = "";
+    public String OUTNAME = "";
+    public String FECPROC = "";
+    public String STAT = "";
+    public String TABLA = "";
+    public int QTYREG;
+    public int QTY_TICKETS;
+    public int QTY_FOUND;
+    public int IN_QTYREG;
+    public String FASE = "";
+    public String DATE_UPLOAD = "";
+    public String GROUP = "";
+    public String IN_TICKET = "";
+    public String A3096CCUST = "";
+    public String A3096FUENT = "";
+    public String A3096TENVI = "";
+    public String A3096TPROC = "";
+    public String A3096PAIS = "";
+    public String A3096FCARG = "";
+    public String A3096TKT = "";
+    public String A3096FPROB = "";
+    public String A3096DSEND = "";
+    public String A3096CDCOM = "";
+    public String A3096CMTD = "";
+    public String A3096FGRPO = "";
+    public String A3096FASIG = "";
+    public String A3096HASIG = "";
+    public String A3096FECAC = "";
+    public String A3096HORAC = "";
+    public String statusDate = "";
+    public String date = "";
+    public String group = "";
+    public String IN_CCUST = "";
+    public String IN_FUENT = "";
+    public String IN_TENVI = "";
+    public String IN_TPROC = "";
+    public String IN_PAIS = "";
+    public String IN_FCARG = "";
+    public String IN_FCARGA = "";
+    public String IN_SCOUNTRY = "";
+    public String TICKET = "";
+    public String SAGENT = "";
+    public String PNAME = "";
+    public String SCOUNTRY = "";
+    public String STVAL = "";
+    public String IN_APLICA = "";
+    public String IN_SEQNUM = "";
+    public String IN_DENV = "";
+    public String IN_INPNAME = "";
+    public String IN_NETDIR = "";
+    public String IN_INPEXTE = "";
+    public String IN_INPDESC = "";
+    public String IN_INPTYPE = "";
+    public String IN_LIBNAME = "";
+    public String IN_OUTNAME = "";
+    public String IN_FECPROC = "";
+    public String IN_STAT = "";
+    public String IN_TABLA = "";
+    public String IN_FASE = "";
+    public String IN_DATE_FROM = "";
+    public String IN_DATE_TO = "";
+    public String IN_APLICATION = "";
+    
+    
+    
 }
