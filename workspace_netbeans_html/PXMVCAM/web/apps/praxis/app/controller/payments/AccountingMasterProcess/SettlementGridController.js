b0c7648e9e414fdbef8615f5b0a40dc840ccf1b9
Yamilet Remicio Gonzales <yadiraremiciogonzales@gmail.com>
2025-09-25 18:33:46 -0500

    feat add column tickets

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/AccountingMasterProcess/SettlementGridController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/AccountingMasterProcess/SettlementGridController.js
@@ -139,6 +139,7 @@ Ext.define('Ext.Praxis.controller.payments.AccountingMasterProcess.SettlementGri
                 'Sale Amount ': x.SUMA_VTA,
                 'Sale Currency':x.MDA_VTA,
                 'Qty Ticket':x.QTY_TKT,
+                'Tickets':x.LST_TKT,
                 'Diff': x.SUM_DIFF
             };
             return obj;
