80e7a8ffc33bb780e7cebeab7f3268a58cf3c441
Ftorres-g <82858342+Ftorres-g@users.noreply.github.com>
2025-10-09 13:09:55 -0500

    Error Control Module CSR:829
    Se agrega modulo para el control log errores.

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A4451Filter.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.miatech.praxis.payment.filter;
+
+import net.miatech.beans.Pagination;
+
+/**
+ *
+ * @author ftorres
+ */
+public class A4451Filter {
+    
+    public Pagination page = new Pagination();
+    
+//    public double totA4183PASRV = 0;
+    
+    
+    public String A4451KEY1 = "";
+    public String A4451KEY2 = "";
+    public String A4451KEY3 = "";
+//    public String V_CODPRO = "";
+//    public String V_PRDA = "";
+    
+    
+}
