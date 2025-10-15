614a976fbbf2c549af77f880c6d2d983d746b026
avas27027 <57721275+avas27027@users.noreply.github.com>
2025-10-15 18:44:47 -0500

    Fix Merge remote-tracking branch 'origin/Dev_CSR01001' into Dev_Deployment

--- workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A2290Filter.java
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/A2290Filter.java
@@ -1058,6 +1058,7 @@ public class A2290Filter extends A2290 {
     public String HOSEND = "";
     public String FERECV = "";
     public String HORECV = "";
+    public String IN_FECFILTRO  = "";
 
     public String getVariable() {
         return BANDOC;
