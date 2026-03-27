3d77bdc80227ce0fbe6efbd2c21ecef9ef09c984
usr_mi_bsaune-at-119260075650 <bsaune@miatech.net>
2026-03-26 19:42:55 -0500

    changes to IND 2709

--- workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/payments/BankReconciliationLogic.java
+++ workspace_netbeans_html/PXMVCAM/src/java/net/miatech/praxis/logic/payments/BankReconciliationLogic.java
@@ -111,32 +111,12 @@ public class BankReconciliationLogic {
         return bankReconciliationDAO.loadPX269MPS287(filters, user);
     }
     
-    public String loadPX269SQP05117_REFND(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
-        return bankReconciliationDAO.loadPX269SQP05117_REFND(filters, user);
-    }
-    
-    public String loadPX269SQP05117_CHGBAK(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
-        return bankReconciliationDAO.loadPX269SQP05117_CHGBAK(filters, user);
-    }
-    
-    public String loadPX269SQP05117_ACREDIT(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
-        return bankReconciliationDAO.loadPX269SQP05117_ACREDIT(filters, user);
+    public String loadPX269SQP05117_REFND(A2290Filter filter, UserView user) throws SQLException, Exception {
+        return bankReconciliationDAO.loadPX269SQP05117_REFND(filter, user);
     }
 
     public String loadPX269SQP05117OnlyLiq(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
         return bankReconciliationDAO.loadPX269SQP05117OnlyLiq(filters, user);
-    }
-    
-    public String loadPX269SQP05117OnlyLiq_REFND(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
-        return bankReconciliationDAO.loadPX269SQP05117OnlyLiq_REFND(filters, user);
-    }
-    
-    public String loadPX269SQP05117OnlyLiq_CHGBAK(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
-        return bankReconciliationDAO.loadPX269SQP05117OnlyLiq_CHGBAK(filters, user);
-    }
-    
-    public String loadPX269SQP05117OnlyLiq_ACREDIT(List<A2290Filter> filters, UserView user) throws SQLException, Exception {
-        return bankReconciliationDAO.loadPX269SQP05117OnlyLiq_ACREDIT(filters, user);
     }
 
     public List<A2290Filter> loadPX269SQP00869(A2290Filter filter) throws SQLException, Exception {
