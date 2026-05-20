419f2218a1940254c72001450cf3b90327771ec3
zenobio86 <zenobio86@gmail.com>
2026-05-20 10:41:11 -0500

    zpp cambios en la excel agrego monto y currency

--- workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/DownloadThePaymentFiles/DownloadThePaymentFilesController.js
+++ workspace_netbeans_html/PXMVCAM/web/apps/praxis/app/controller/payments/DownloadThePaymentFiles/DownloadThePaymentFilesController.js
@@ -86,7 +86,7 @@ Ext.define('Ext.Praxis.controller.payments.DownloadThePaymentFiles.DownloadThePa
                 ["SAFE", "SAFETYPAY"],
                 ["CODE", "CODENSA"],
                 ["GYE", "GUAYAQUIL"],
-                ["PACI", "PACIFICAR"],
+                ["PACI", "PACIFICAR"]
             ]
         }));
         CmbTypeprocesa.setValue("");
