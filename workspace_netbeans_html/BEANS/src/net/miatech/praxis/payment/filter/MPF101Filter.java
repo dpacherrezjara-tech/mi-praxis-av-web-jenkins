0938b70046a36891dde1af676a6838e1abc12867
DIEGO <dpacherrez@miatech.net>
2026-08-21 10:07:24 -0500

    Fix-Merge branch 'Dev_IND03153' into Prod_Deployment_v5

---
+++ workspace_netbeans_html/BEANS/src/net/miatech/praxis/payment/filter/MPF101Filter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.miatech.praxis.payment.filter;
+
+import net.miatech.praxis.payment.MPF101;
+
+/**
+ *
+ * @author ftorres
+ */
+public class MPF101Filter extends MPF101{
+    
+    
+    public String  IN_AGENT    ="";  
+    public String  IN_DATEF     ="";  
+    public String  IN_DATET    ="";  
+    
+}
